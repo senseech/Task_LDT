616d97c11a2d58ff959ee0acccc0fb6ec73c5d50
senseech <maxic1234@gmail.com>
2022-07-17 21:31:35 +0300

    formatCode

--- src/main/java/TestYandex.java
+++ src/main/java/TestYandex.java
@@ -18,40 +18,47 @@ public class TestYandex {
         Actions actions = new Actions(driver);
 
         try {
+
 //          WebDriver в папке проекта
             System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
             driver.get("https://yandex.ru/");
 
-//          переход в маркет
-            WebElement element_market = new WebDriverWait(driver, Duration.ofSeconds(dur)).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@data-statlog='services_new.item.market.0']")));
+//          Переход в маркет
+            WebElement element_market = new WebDriverWait(driver, Duration.ofSeconds(dur))
+                    .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@data-statlog='services_new.item.market.0']")));
             String URL_market = element_market.getAttribute("href");
             String[] split_URL_market = URL_market.split("=");
             driver.get(split_URL_market[0]);
 
-//          принять куки
-            WebElement element_acceptCookie = new WebDriverWait(driver, Duration.ofSeconds(dur)).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//table[@data-id=\"table\"]//button[@data-text=\"Принять все\"]")));
+//          Принять куки
+            WebElement element_acceptCookie = new WebDriverWait(driver, Duration.ofSeconds(dur))
+                    .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//table[@data-id=\"table\"]//button[@data-text=\"Принять все\"]")));
             element_acceptCookie.click();
 
-//          переход в электронику
-            WebElement element_menu = new WebDriverWait(driver, Duration.ofSeconds(dur)).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@data-zone-name='category-link']//a[@href='/catalog--elektronika/54440']")));
+//          Переход в электронику
+            WebElement element_menu = new WebDriverWait(driver, Duration.ofSeconds(dur))
+                    .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@data-zone-name='category-link']//a[@href='/catalog--elektronika/54440']")));
             String URL_menu = element_menu.getAttribute("href");
             driver.get(URL_menu);
 
-//          переход в телефоны
-            WebElement element_phones = new WebDriverWait(driver, Duration.ofSeconds(dur)).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@data-zone-name='link']//a[text()='Смартфоны']")));
+//          Переход в телефоны
+            WebElement element_phones = new WebDriverWait(driver, Duration.ofSeconds(dur))
+                    .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@data-zone-name='link']//a[text()='Смартфоны']")));
             String URL_phones = element_phones.getAttribute("href");
             driver.get(URL_phones);
 
-//          переход в расширенный поиск
-            WebElement element_filters = new WebDriverWait(driver, Duration.ofSeconds(dur)).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='_3RoU0']//a[@data-auto='allFiltersButton']")));
+//          Переход в расширенный поиск
+            WebElement element_filters = new WebDriverWait(driver, Duration.ofSeconds(dur))
+                    .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='_3RoU0']//a[@data-auto='allFiltersButton']")));
             String URL_filters = element_filters.getAttribute("href");
             driver.get(URL_filters);
 
-//          фильтр до 20к р
-            WebElement element_price = new WebDriverWait(driver, Duration.ofSeconds(dur)).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@data-prefix=\"до\"]//input")));
+//          Фильтр до 20к₽
+            WebElement element_price = new WebDriverWait(driver, Duration.ofSeconds(dur))
+                    .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@data-prefix=\"до\"]//input")));
             element_price.sendKeys("20000");
 
-            //5 checkboxes в расширенном поиске
+//          5 checkboxes в расширенном поиске
             actions.sendKeys(Keys.PAGE_DOWN).perform();
             Thread.sleep(dur * 1000);
             List<WebElement> element_checkboxes = driver.findElements(By.xpath("//div[@data-filter-id=\"7893318\"]//label[@class=\"cyT3Q\"]//div[@class=\"_1WMsA _1VtMI _176_6\"]"));
@@ -60,35 +67,40 @@ public class TestYandex {
             }
 
 //          5 дюймов
-            WebElement element_screenSize = new WebDriverWait(driver, Duration.ofSeconds(dur)).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class=\"_3i0uk\"]//h4[contains(text(),'Диагональ экрана (точно)')]")));
+            WebElement element_screenSize = new WebDriverWait(driver, Duration.ofSeconds(dur))
+                    .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class=\"_3i0uk\"]//h4[contains(text(),'Диагональ экрана (точно)')]")));
             element_screenSize.click();
             List<WebElement> element_inputs = driver.findElements(By.xpath("//div[@class=\"_3i0uk\"]//div[@data-prefix=\"от\"]//input[@class=\"_2xtC2\"]"));
             element_inputs.get(1).sendKeys("5");
 
-//          отображение результата сортировки
-            WebElement element_searchResult = new WebDriverWait(driver, Duration.ofSeconds(dur)).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[contains(text(),'Показать')]")));
+//          Отображение результата сортировки
+            WebElement element_searchResult = new WebDriverWait(driver, Duration.ofSeconds(dur))
+                    .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[contains(text(),'Показать')]")));
             String URL_searchResult = element_searchResult.getAttribute("href");
             driver.get(URL_searchResult);
 
-//          первый элемент в списке
-            WebElement element_firstResult = new WebDriverWait(driver, Duration.ofSeconds(dur)).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@data-index=\"1\"]//a")));
+//          Первый элемент в списке
+            WebElement element_firstResult = new WebDriverWait(driver, Duration.ofSeconds(dur))
+                    .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@data-index=\"1\"]//a")));
             String URL_firstResult = element_firstResult.getAttribute("href");
             Thread.sleep(dur * 1000);
 
-//          количество выведенных результатов
+//          Количество выведенных результатов
             List<WebElement> element_fullResults = driver.findElements(By.xpath("//div[@style=\"overflow-anchor: none;\"]"));
             System.out.println("Отображено " + (element_fullResults.size()) + " результатов");
 
-//          смена сортировки
-            WebElement element_sortByPrice = new WebDriverWait(driver, Duration.ofSeconds(dur)).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[text()='по цене']")));
+//          Смена сортировки на цену
+            WebElement element_sortByPrice = new WebDriverWait(driver, Duration.ofSeconds(dur))
+                    .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[text()='по цене']")));
             element_sortByPrice.click();
             Thread.sleep(dur * 1000);
 
-//          переходим по первому элементу в списке
+//          Переход по первому элементу в списке
             driver.get(URL_firstResult);
 
-//          вывод оценки первого элемента
-            WebElement element_rating = new WebDriverWait(driver, Duration.ofSeconds(dur)).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='_1EOgH _2I6wc _1NfPD']//span")));
+//          Вывод оценки первого элемента
+            WebElement element_rating = new WebDriverWait(driver, Duration.ofSeconds(dur))
+                    .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='_1EOgH _2I6wc _1NfPD']//span")));
             String rating = element_rating.getAttribute("innerText");
             System.out.println("Рейтинг первого элемента " + rating);
             Thread.sleep(dur * 1000);
@@ -97,7 +109,7 @@ public class TestYandex {
             e.printStackTrace();
 
         } finally {
-//          закрытие сессии
+//          Закрытие сессии
             driver.quit();
         }
     }
